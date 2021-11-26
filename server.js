9705e85f2c6807ef0b0e76345ec32b99bbb4e24e
Caroline P. da Silva <carolinepedasil@gmail.com>
2021-11-25 22:56:29 -0300

    Aula 3 - Criando as primeiras rotas (POST e GET)

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-require('dotenv').config()
-const { app } = require("./src/app")
-const port = process.env.PORT || 3000
+require("dotenv").config();
+const { app } = require("./src/app");
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`Servidor está rodando na porta ${port}`)
-})
+  console.log(`Servidor está rodando na porta ${port}`);
+});
--- src/app.js
+++ src/app.js
@@ -1,17 +1,20 @@
-const express = require("express")
-const app = express()
-require("./models/Doctor");
+const express = require("express");
+const app = express();
+ 
 
-app.use(express.json())
+app.use(express.json());
+
+const index = require('./routes/index')
+const doctors = require('./routes/doctors')
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    )
-    next()
-})
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.options("/*", (req, res) => {
     res.header("Access-Control-Allow-Origin", "*")
@@ -22,7 +25,8 @@ app.options("/*", (req, res) => {
     );
     res.send("send some thing whatever")
 })
-
+app.use("/", index)
+app.use("/doctors", doctors)
 module.exports = {
 	app
 }
